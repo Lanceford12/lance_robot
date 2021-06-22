ecbbc66bca1ff44bc6d192982528cd6827049a39
Jeremy James <romarjeremy@gmail.com>
2021-06-21 20:59:52 -0500

    add spelling checking

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from tkinter import *
 import ttsm
 import PyPDF2
 import threading
 from tkinter import filedialog
 import nltk
-
+import spell_checker
 
 root = tk.Tk()
 root.geometry("600x700")
@@ -19,6 +20,11 @@ roboti=tk.PhotoImage(file="images/robot.png")
 heading = tk.Label(root,fg="black",text="LIZ TEXT TO SPEECH",font = "Times 16 bold italic")
 heading.pack()
 
+# Load 1000 common english words from the text file
+def load_words():
+    with open("word_database.txt") as json_file:
+        valid_words = set(json_file.read().split())
+    return valid_words
 
 def menu():
     
@@ -31,6 +37,10 @@ def menu():
     global textarea
     textarea = tk.Text(root,height=10,width=50,wrap=tk.WORD)
     textarea.pack()
+
+    # Testing spelling suggestions
+    english_words = load_words()
+    textarea.insert(INSERT, spell_checker.check_spelling(["Adam", "aandd", "mary"], english_words=english_words))
     
     convert_button = tk.Button(root,bg="aqua",text="convert to speech",font = "Times 15 bold italic",command=lambda:speak(textarea.get("1.0","end")))
     convert_button.pack()
@@ -80,17 +90,11 @@ def read(file):
          pdfFileObj.close()
      
 
-
-    
-   
-
-    
-
 def main():
     menu()
 
-    x=threading.Thread(target=ttsm.speak,args=("Hey there, my name is lance, how are you",))
-    x.start()
+    #x=threading.Thread(target=ttsm.speak,args=("Hey there, my name is lance, how are you",))
+    #x.start()
     
     
     root.mainloop()
